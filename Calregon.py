ebc47d18addced24285a6a9fede3a1e31a20c156
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2023-04-16 02:23:48 +0300

    Adding mute

--- Calregon.py
+++ Calregon.py
@@ -1,20 +1,22 @@
 import os
-#from collections.abc import Mapping
-from sound import Sound
+
 import subprocess
 from tkinter import *
 import tkinter as tk
 
-import ctypes
-MUTE = 0x8000
-SND_app = 0x80
-ctypes.windll.winmm.waveOutSetVolume(0,MUTE)
-#Sound.mute()
+####What is tk. ???!?!?!?
+#### what diffrant . and =  ????!?!?
 
-exit()
+def add_num():
+   num1 = int(Ent1.get())
+   result = num1 * 3.75
+   ansr.config(text="result"+str(result))
+
 def open_app(path):
     os.startfile(path)
     
+
+
 wind = Tk()
 wind.title("Mixr ")
 wind.geometry("500x500+10+500")
@@ -23,10 +25,8 @@ title = Label(wind , text='Mixr',fg='gold', bg='black' , font=('tajawal ', 16,'b
 title.pack(fill=X)
 
 
-Edgebutton  = tk.Button(wind,bg='black', fg='white', text='Edge', command= lambda: open_app(r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'))
-Edgebutton.pack()
+xbutton  = tk.Button(wind,bg='black', fg='white', text='Open EDGE', command= lambda: open_app(r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'))
 
-Steambutton  = tk.Button(wind,bg='black', fg='white', text='Steam', command= lambda: open_app(r'C:\Program Files (x86)\Steam\steam.exe'))
-Steambutton.pack()
+xbutton.pack()
 
 wind.mainloop()
--- Mute.py
+++ Mute.py
@@ -1,27 +1,3 @@
-import sys
-import subprocess
+import os
 
-mute_command   = 'amixer sset Master mute'
-unmute_command = 'amixer sset Master unmute'
-
-def sys_message(msg):
-    subprocess.call(['notify-send', '-t', '1000', '{}'.format(msg)])
-    
-process = subprocess.Popen(['amixer sget Master'], shell=True, stdout=subprocess.PIPE)
-#output= process.communicate()[0].split("\n")
-print(process.communicate()[0])
-exit()
-is_mute = False
-
-for line in output:
-    data = line.lower()    
-    if ('front left' in data) and '[off]' in data:
-        is_mute = True        
-if is_mute:
-    p = subprocess.Popen(['amixer sset Master unmute'], shell=True, stdout=subprocess.PIPE)  
-    sys_message("Mute foi desativado") #mute was disabled
-else:    
-    p = subprocess.Popen(['amixer sset Master mute'], shell=True, stdout=subprocess.PIPE) 
-    sys_message("Mute foi ativado") #mute was enabled
-    
-sys.exit(0)
+os.system(r"C:\Users\mesh3\nircmd-x64\nircmd.exe mutesysvolume 1")
