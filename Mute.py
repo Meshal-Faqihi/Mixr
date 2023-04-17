47e4161f38b0df44d6d957ab95ea5b3b0a5412b2
Meshal <mesh3ll07@gmail.com>
2023-04-17 04:42:55 +0300

    ADD Button for Microphone its work but want Fix

--- Mute.py
+++ Mute.py
@@ -14,13 +14,26 @@ def Mute():
    interface = devices.Activate(IAudioEndpointVolume._iid_ , CLSCTX_ALL, None)
    volume = cast(interface, POINTER(IAudioEndpointVolume))
    unvolume = cast(interface, POINTER(IAudioEndpointVolume))
-   global oneButton_on
-   oneButton_on = not oneButton_on
-   if oneButton_on:
+   global Toggel_Button
+   Toggel_Button = not Toggel_Button
+   if Toggel_Button:
       volume.SetMute(0, None)
    else:
       unvolume.SetMute(1, None)
-oneButton_on = True
+Toggel_Button = True
+
+def Mic():
+   devices = AudioUtilities.GetMicrophone()
+   interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+   volume = cast(interface, POINTER(IAudioEndpointVolume))
+   unvolume = cast(interface, POINTER(IAudioEndpointVolume))
+   global Toggel_Button
+   Toggel_Button = not Toggel_Button
+   if Toggel_Button:
+      volume.SetMute(0, None)
+   else:
+      unvolume.SetMute(1, None)
+Toggel_Button = True
 
 wind = Tk()
 wind.title("Mixr ")
@@ -35,7 +48,10 @@ Edgebutton.pack()
 Steambutton  = tk.Button(wind,bg='black', fg='white', text='Steam', command= lambda: open_app(r'C:\Program Files (x86)\Steam\steam.exe'))
 Steambutton.pack()
 
-Mute  = tk.Button(wind,bg='black', fg='white', text='Mute', command= Mute)
-Mute.pack()
+Speakers = tk.Button(wind,bg='black', fg='white', text='MuteAudio', command= Mute)
+Speakers.pack()
+
+Microphone  = tk.Button(wind,bg='black', fg='white', text='MuteMic', command= Mic)
+Microphone.pack()
 
 wind.mainloop()
